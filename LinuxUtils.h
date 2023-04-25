d3d84db4fd59958218e2bbdf1e52af72b3d0a9c4
Erarnitox <david@erarnitox.de>
2023-04-25 04:02:43 +0200

    execute child process

--- LinuxUtils.h
+++ LinuxUtils.h
@@ -1,15 +1,78 @@
 #pragma once
 
+#include <cstdarg>
+#include <cstdlib>
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 #include <dirent.h>
 #include <unistd.h>
 #include <ftw.h>
+#include <fcntl.h>
+#include <sys/wait.h>
 
 static char* copy_to_path;
 static char* copy_from_path;
 static bool copy_busy = false;
+
+int exec_command(char* exe, char* log_file, ...) {
+    int pipe_fds[2];
+
+    if(pipe(pipe_fds)) return -1;
+
+    pid_t pid = fork();
+
+    if(pid == -1) return -1;
+
+    if(pid == 0) {
+        char* args[32] = {0};
+
+        dup2(pipe_fds[1], STDOUT_FILENO);
+        dup2(pipe_fds[1], STDERR_FILENO);
+
+        close(pipe_fds[1]);
+
+        va_list valist;
+        va_start(valist, log_file);
+
+        char* p = 0;
+        for(int i=1; p = va_arg(valist, char*); ++i) {
+            if( i > 32) break;
+
+            args[i] = p;
+        }
+        va_end(valist);
+        execvp(exe, args);
+        exit(-1);
+    }
+    
+    char read_buffer[1024];
+    size_t r = 0;
+    int log_fd = 0;
+
+    close(pipe_fds[1]);
+
+    if(log_file) {
+        log_fd = open(log_file, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+    }
+
+    while((r = read(pipe_fds[0], read_buffer, sizeof(read_buffer)))) {
+        write(STDOUT_FILENO, read_buffer, r);
+
+        if(log_fd)
+            write(log_fd, read_buffer, r);
+    }
+
+    if(log_fd)
+        close(log_fd);
+
+    close(pipe_fds[0]);
+
+    int status = -1;
+    waitpid(pid, &status, 0);
+
+    return WEXITSTATUS(status);
+}
 
 bool copy_file(const char* from, char* to) {
     FILE* ff = fopen(from, "r");
@@ -59,3 +122,5 @@ bool copy_dir_contents(char* path, char* to) {
     copy_busy = false;
     return ret == 0;
 }
+
+
